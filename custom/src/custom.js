6e649b45d71ffa6ddf43b06332181bea6a2a2b95
andyinformatika23-hash <andy.informatika23@gmail.com>
2026-07-27 15:41:03 +0700

    copy index

--- custom/src/custom.js
+++ custom/src/custom.js
@@ -4,7 +4,8 @@ import "./todo-list.js";
 // import "./lib/explode-quiz.js";
 // import "./lib/attendance-system.js";
 // import "./quiz-dashboard-lite.js";
-import "./full-quiz-dashboard.js";
+// import "./full-quiz-dashboard.js";
 // import "./explode-quiz-dashboard.js";
-
 import "./quiz-dashboard-lite2/quiz-dashboard-lite2.js";
+
+// import '@smartedu/quiz-dashboard-lite2/quiz-dashboard-lite2.js'
